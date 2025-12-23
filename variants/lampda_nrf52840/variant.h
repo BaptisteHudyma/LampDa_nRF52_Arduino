f3bb1de9cc51bf7bce4002af6f9e7cb30f487311
BaptHdm <bapt.hudyma95650@gmail.com>
2025-12-23 23:35:26 +0100

    Bump lampda library version

--- variants/lampda_nrf52840/variant.h
+++ variants/lampda_nrf52840/variant.h
@@ -20,7 +20,7 @@ extern "C" {
 #endif  // __cplusplus
 
 #define LAMPDA_FIRMWARE_VERSION_MAJOR 1
-#define LAMPDA_FIRMWARE_VERSION_MINOR 2
+#define LAMPDA_FIRMWARE_VERSION_MINOR 3
 
 #define ADC_RESOLUTION (12) // 12 bit ADC
 
